9d99889d9bbf4e50a68d923245b130b02d025ca5
Damon Revoe <him@revl.org>
2019-10-18 09:53:09 -0400

    Update run_backend_locally.sh to watch processes

--- run_backend_locally.sh
+++ run_backend_locally.sh
@@ -11,15 +11,14 @@ for var in UMLS_EMAIL UMLS_USERNAME UMLS_PASSWORD; do
     fi
 done
 
-if ! test -d .ve; then
-    virtualenv -p python3 .ve
-    . ./.ve/bin/activate
-    pip install flask gunicorn
-fi
+test -d .ve || virtualenv -p python3 .ve
+. ./.ve/bin/activate
+pip install flask gunicorn
 
 METAMAP_CLIENT_JAR="`pwd`/metamap_client.jar"
+JOB_INPUT_DIR="`pwd`/job_input"
 JOB_OUTPUT_DIR="`pwd`/job_output"
-export METAMAP_CLIENT_JAR JOB_OUTPUT_DIR
+export METAMAP_CLIENT_JAR JOB_INPUT_DIR JOB_OUTPUT_DIR
 
 if ! test -f "$METAMAP_CLIENT_JAR"; then
     echo "$METAMAP_CLIENT_JAR does not exist" >&2
@@ -27,8 +26,42 @@ if ! test -f "$METAMAP_CLIENT_JAR"; then
     exit 2
 fi
 
-mkdir -p "$JOB_OUTPUT_DIR"
+mkdir -p "$JOB_INPUT_DIR" "$JOB_OUTPUT_DIR"
 
-. ./.ve/bin/activate
-#python backend/backend.py
-gunicorn -w 4 -b :8080 backend.backend:app
+cleanup()
+{
+    kill "$backend_pid" "$worker_pid" 2>/dev/null
+}
+
+trap cleanup EXIT
+
+echo 'Starting web backend...'
+gunicorn -w 4 -b :8080 backend.backend:app > backend.log 2>&1 &
+backend_pid=$!
+
+
+echo 'Starting metamap_worker...'
+java -jar metamap_worker/metamap_worker.jar > worker.log 2>&1 &
+worker_pid=$!
+
+check_child() {
+    local service=$1
+    local pid=$2
+
+    if [ -z "$pid" ]; then
+        return
+    fi
+
+    if ! kill -0 $pid 2> /dev/null; then
+        echo "$service unexpectedly stopped." >&2
+        echo "See `pwd`/$service.log for details." >&2
+        exit 3
+    fi
+}
+
+while true; do
+    check_child 'backend' $backend_pid
+    check_child 'worker' $worker_pid
+
+    sleep .1
+done
